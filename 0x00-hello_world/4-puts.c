17daede992c1e3b8275f41de255eaf6bef089005
root <mariandege98@gmail.com>
2022-03-10 10:18:37 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,20 @@
 #!/bin/bash
-gcc -E $CFILE -o c
+/*
+ * File: 4-puts.c
+ * Auth: Maria
+ * */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
